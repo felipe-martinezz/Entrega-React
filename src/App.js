975376a95f8fd8b71bf4838ef7c7c6e3f1dba25a
Felipe Martinez <felimartinez01@hotmail.com>
2022-09-13 13:56:21 -0300

    generando un menú

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from "./components/Footer";
 
 function App() {
+  let styleH1 = {
+    color: "brown",
+    textDecoration: "underline",
+    marginTop: "100px",
+  };
+
+  let title = "E-Commerce React"
+
   return (
     <div className="App">
+      <Footer />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1 style={styleH1}>{title}</h1>
       </header>
     </div>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import "./footer.css";
+
+function Footer() {
+    return (
+        <footer className="footer">
+          <h2>
+            <strong>Mi E-Commerce</strong>   
+          </h2>  
+          <ul>
+            <li>Inicio</li>
+            <li>Productos</li>
+            <li>Contactos</li>
+          </ul>
+        </footer>
+    );
+}
+
+export default Footer;
